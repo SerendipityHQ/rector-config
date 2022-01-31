4335d357f650914e9d75501abd3d5f5553b15094
Aerendir <hello@aerendir.me>
2022-01-31 17:57:25 +0100

    Add a configuration for Symfony Bundles.

--- src/SerendipityHQ.php
+++ src/SerendipityHQ.php
@@ -28,6 +28,25 @@ class SerendipityHQ
         \Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector::class,
         \Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector::class,
     ];
+    public const SHQ_SYMFONY_BUNDLE = __DIR__ . '/Sets/symfony-bundle.php';
+    public const SHQ_SYMFONY_BUNDLE_SKIP = [
+        \Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
+        \Rector\CodeQuality\Rector\Concat\JoinStringConcatRector::class,
+        \Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector::class,
+        \Rector\CodingStyle\Rector\Class_\AddArrayDefaultToArrayPropertyRector::class,
+        \Rector\CodingStyle\Rector\ClassConst\VarConstantCommentRector::class,
+        \Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector::class,
+        \Rector\CodingStyle\Rector\ClassMethod\RemoveDoubleUnderscoreInMethodNameRector::class,
+        \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
+        \Rector\CodingStyle\Rector\Switch_\BinarySwitchToIfElseRector::class,
+        \Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector::class,
+        \Rector\Php56\Rector\FunctionLike\AddDefaultValueForUndefinedVariableRector::class, // Maybe good one day
+        \Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector::class,
+        \Rector\PHPUnit\Rector\ClassMethod\AddDoesNotPerformAssertionToNonAssertingTestRector::class,
+        \Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\AddArrayParamDocTypeRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\AddArrayReturnDocTypeRector::class,
+    ];
     public const SHQ_SYMFONY_APP = __DIR__ . '/Sets/symfony-app.php';
     public const SHQ_SYMFONY_APP_SKIP = [
         \Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector::class,
---
+++ src/Sets/symfony-bundle.php
@@ -0,0 +1,47 @@
+<?php
+
+declare (strict_types=1);
+namespace SerendipityHQ\Integration\Rector\Sets;
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+return static function (ContainerConfigurator $containerConfigurator) : void {
+    $containerConfigurator->import(SetList::ACTION_INJECTION_TO_CONSTRUCTOR_INJECTION);
+    $containerConfigurator->import(SetList::CODE_QUALITY);
+    $containerConfigurator->import(SetList::CODING_STYLE);
+    // $containerConfigurator->import(SetList::DEAD_CODE);
+    // $containerConfigurator->import(SetList::EARLY_RETURN);
+    // $containerConfigurator->import(SetList::FLYSYSTEM_20);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_40);
+    $containerConfigurator->import(SetList::FRAMEWORK_EXTRA_BUNDLE_50);
+    $containerConfigurator->import(SetList::MONOLOG_20);
+    // $containerConfigurator->import(SetList::NAMING);
+    // $containerConfigurator->import(SetList::ORDER);
+    $containerConfigurator->import(SetList::PHP_52);
+    $containerConfigurator->import(SetList::PHP_53);
+    $containerConfigurator->import(SetList::PHP_54);
+    $containerConfigurator->import(SetList::PHP_56);
+    $containerConfigurator->import(SetList::PHP_70);
+    $containerConfigurator->import(SetList::PHP_71);
+    $containerConfigurator->import(SetList::PHP_72);
+    $containerConfigurator->import(SetList::PHP_73);
+    // $containerConfigurator->import(SetList::PHP_74);
+    // $containerConfigurator->import(SetList::PHP_80);
+    // $containerConfigurator->import(SetList::PHP_81);
+    // $containerConfigurator->import(SetList::PRIVATIZATION);
+    // $containerConfigurator->import(SetList::PSR_4);
+    $containerConfigurator->import(SetList::TYPE_DECLARATION);
+    // $containerConfigurator->import(SetList::TYPE_DECLARATION_STRICT);
+    $containerConfigurator->import(SetList::UNWRAP_COMPAT);
+
+    $services = $containerConfigurator->services();
+    $services->set(\Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector::class);
+    $services->set(\Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector::class);
+    $services->set(\Rector\DeadCode\Rector\Node\RemoveNonExistingVarAnnotationRector::class);
+
+    $parameters = $containerConfigurator->parameters();
+    $parameters->set(Option::AUTO_IMPORT_NAMES, true);
+    $parameters->set(Option::IMPORT_DOC_BLOCKS, true);
+    $parameters->set(Option::IMPORT_SHORT_CLASSES, false);
+};
